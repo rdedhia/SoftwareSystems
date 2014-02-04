e5dde142b692b98990a96f0197ed85da0e726e67
Rahil Dedhia <rahil.dedhia@students.olin.edu>
2014-02-04 01:41:25 -0500

    added cards.c to hw1

--- lecture04/aspace.c
+++ lecture04/aspace.c
@@ -10,15 +10,25 @@ License: GNU GPLv3
 
 int global;
 
+void foo () {
+	int local2 = 2;
+	printf("Address of local2 is %p\n", &local2);
+}
+
 int main ()
 {
     int local = 5;
     void *p = malloc(128);
+    void *m = malloc(20);
+    void *n = malloc(20);
 
     printf ("Address of main is %p\n", main);
     printf ("Address of global is %p\n", &global);
     printf ("Address of local is %p\n", &local);
+    foo();
     printf ("Address of p is %p\n", p);
-    
+    printf ("Address of m is %p\n", m);
+    printf ("Address of n is %p\n", n);
+
     return 0;
 }
