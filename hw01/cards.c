66b1196a77ac1eb1c9f47d6be47d4a547aa9e621
Rahil Dedhia <rahil.dedhia@students.olin.edu>
2014-02-05 02:05:21 -0500

    split cards.c into functions

--- hw01/cards.c
+++ hw01/cards.c
@@ -1,37 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void inputHandler(char *card, int *val) {
+	switch(*card) {
+	case 'K':
+	case 'Q':
+	case 'J':
+		*val = 10;
+		break;
+	case 'A':
+		*val = 11;
+		break;
+	case 'X':
+		break;
+	default:
+		*val = atoi(card);
+		if ((*val < 1) || (*val > 10)) {
+			puts("I don't understand that value!");
+			break;
+		}
+	}
+}
+
+void counter(char *card, int *count) {
+	int val = 0;
+	inputHandler(card, &val);
+	if ((val > 2) && (val < 7)) {
+		*count = *count + 1;
+	} else if (val == 10) {
+		*count = *count - 1;
+	}
+	printf("Current count: %i\n", *count);
+}
+
 int main() {
 	char card_name[3];
 	int count = 0;
 	while (card_name[0] != 'X') {
 		puts("Enter the card name: ");
 		scanf("%2s", card_name);
-		int val = 0;
-		switch(card_name[0]) {
-			case 'K':
-			case 'Q':
-			case 'J':
-				val = 10;
-				break;
-			case 'A':
-				val = 11;
-				break;
-			case 'X':
-				continue;
-			default:
-				val = atoi(card_name);
-				if ((val < 1) || (val > 10)) {
-					puts("I don't understand that value!");
-					continue;
-				}
-		}
-		if ((val > 2) && (val < 7)) {
-			count++;
-		} else if (val == 10) {
-			count--;
-		}
-		printf("Current count: %i\n", count);
+		counter(&card_name, &count);
 	}
 	return 0;
 }
