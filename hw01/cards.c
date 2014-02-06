2b5daec42adc4407c78fc66cb49b16290603a1b8
Rahil Dedhia <rahil.dedhia@students.olin.edu>
2014-02-06 02:14:55 -0500

    commented cards.c

--- hw01/cards.c
+++ hw01/cards.c
@@ -1,7 +1,18 @@
+/*Modified code from card_counter.c in Head First C 
+by Griffiths and Griffiths
+
+Code is now split into three functions
+
+Modified by Rahil Dedhia
+Software Systems Homework 01*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
 void inputHandler(char *card, int *val) {
+	/* Takes the user input card and calculates its value
+	if it is a valid input. Otherwise, gives an
+	error message to the user */
 	switch(*card) {
 	case 'K':
 	case 'Q':
@@ -23,6 +34,10 @@ void inputHandler(char *card, int *val) {
 }
 
 void counter(char *card, int *count) {
+	/* Takes card value from the inputHandler function, 
+	increments or decrements the count accordingly, and
+	then prints the count.
+	*/
 	int val = 0;
 	inputHandler(card, &val);
 	if ((val > 2) && (val < 7)) {
@@ -34,6 +49,12 @@ void counter(char *card, int *count) {
 }
 
 int main() {
+	/* Main function - contains while loop which prompts
+	user for a card name until the user submits 'X'. User input
+	must be limited to 2 characters. Then calls the counter
+	function to increment or decrement the count depending
+	on the card value.
+	*/
 	char card_name[3];
 	int count = 0;
 	while (card_name[0] != 'X') {
